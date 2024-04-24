13f26541a72dd8f29b2f93525c82e323e389310a
Faiq Dogar <136640929+Faiq-Dogar@users.noreply.github.com>
2024-04-24 10:06:59 +0500

    SignIn and Hero Section

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,83 +1,22 @@
-import Image from "next/image";
+import { Route, Routes } from "react-router-dom";
+import Link from "next/link.js";
+import Header from "./components/Header.js";
+import Navbar from "./components/Navbar.js";
+import Signin from "./components/signIn.js";
+
+
 
 export default function Home() {
   return (
     <>
-      <div className="flex min-h-screen flex-1 flex-col justify-center px-6 py-12 lg:px-8 pt-4 ">
-        <div className="sm:mx-auto sm:w-full sm:max-w-xl">
-          <Image className="mx-auto" src="/Logo.png" alt="Comapny logo" width={150} height={150}/>
-          <h2 className=" text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Sign in to your account
-          </h2>
-        </div>
-
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-6" action="#" method="POST">
-            <div>
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                Email address
-              </label>
-              <div className="mt-2">
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
-            <div>
-              <div className="flex items-center justify-between">
-                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                  Password
-                </label>
-                <div className="text-sm">
-                  <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                    Forgot password?
-                  </a>
-                </div>
-              </div>
-              <div className="mt-2">
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
-            <div>
-              <button
-                type="submit"
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Sign in
-              </button>
-            </div>
-
-            <p className="text-center">Or</p>
-
-            <div>
-              <button
-              type="button"
-              className="w-full bg-red-600	text-white rounded-md py-2.5 text-sm">Sign in with google</button>
-            </div>
-          </form>
-
-          {/* <p className="mt-10 text-center text-sm text-gray-500">
-            Not a member?{' '}
-            <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-              Start a 14 day free trial
-            </a>
-          </p> */}
-        </div>
-      </div>
+      <Signin />
+       {/* <Navbar />
+      <Header />  */}
+      {/* {typeof window !== "undefined" && (
+        <Routes>
+          <Route path="/sign-in" element={<Signin />} />
+        </Routes>
+      )} */}
     </>
   );
 }
