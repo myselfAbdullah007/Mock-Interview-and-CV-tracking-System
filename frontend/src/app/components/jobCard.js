1fba376e631a5de9e1f0b0be8345795221a2461a
Ali Shahzad <145147788+AliSHAHZAD-007@users.noreply.github.com>
2024-05-08 11:50:22 +0500

    Update jobCard.js

--- frontend/src/app/components/jobCard.js
+++ frontend/src/app/components/jobCard.js
@@ -8,7 +8,7 @@ export default function jobCard() {
     <div className='border border-grey-400 w-2/5 mx-auto px-5 space-y-4'>
             <Image className="mx-auto w-full" src="/Background_2toneBlue-home.png" alt="Comapny logo" width={150} height={150} />
             <div className='font-bold'>Title : <span className='ms-2 font-normal'>Software Engineer</span></div>
-            <div className='font-bold'>Company : <span className='ms-2 font-normal'>GenSol</span></div>
+            <div className='font-bold'>Your Company : <span className='ms-2 font-normal'>GenSol</span></div>
             <div className='font-bold'>Curent Status : <span className='ms-2 font-normal'>Physical</span></div>
             <div className='font-bold'>Address : <span className='ms-2 font-normal'>Kohinoor</span></div>
             <div className='font-bold'>Salary : <span className='ms-2 font-normal'>40k-60k</span></div>
