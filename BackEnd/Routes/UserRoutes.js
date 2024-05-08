dcd73a368af54361ed7eb55bacb01a83a9c18c8a
Abdullah <myselfabdullah007@gmail.com>
2024-05-08 10:16:40 +0500

    Resolved Saving User Profile Error

--- BackEnd/Routes/UserRoutes.js
+++ BackEnd/Routes/UserRoutes.js
@@ -4,7 +4,7 @@ const loginController = require('../controllers/UserControllers/login');
 const signupController = require('../controllers/UserControllers/signup');
 const getUserController = require('../controllers/UserControllers/userData');
 const getUserById = require('../controllers/UserControllers/singleUserData');
-const saveUserProfile = require('../controllers/UserControllers/singleUserData');
+const saveUserProfile = require('../controllers/UserControllers/saveProfile');
 
 
 const UserRouter = express.Router();
@@ -18,6 +18,6 @@ UserRouter.get("/", getUserController);
 // Get User by ID Route
 UserRouter.get("/:id", getUserById);
 // Save User profile
-UserRouter.get("/saveProfile", saveUserProfile);
+UserRouter.post("/saveProfile", saveUserProfile);
 
 module.exports = UserRouter;
